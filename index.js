c749fc05dc88cbca792881a6d69ec743dac22134
Ariel Moise <ariel.m23@gmail.com>
2021-03-11 14:47:44 +0200

    edit and delete options

--- index.js
+++ index.js
@@ -48,28 +48,28 @@ async function collectingStudentsData() {
         response[student.id].city = responsePerUser.city;
         response[student.id].gender = responsePerUser.gender;
         response[student.id].hobby = responsePerUser.hobby;
-        // response[student.id].firstName = responsePerUser.hobby;
-        // response[student.id].hobby = responsePerUser.hobby;
+        response[student.id].editButton = 'Edit';
+        response[student.id].deleteButton = 'Delete';
 
         studentsData[student.id] = response[student.id];
     }
 }
 
+let editedStudent;
 collectingStudentsData().then(() => {
     console.log(studentsData)
     displayingData();
     // deleteRow(0);
-    editRow(0);
+    // editedStudent = editRow(0);
 })
 
-
-function displayingData(){
+function displayingData() {
     studentsContainer.innerHTML = '';
 
     let tr = document.createElement('tr');
 
-    tr.innerHTML = 
-    `<td data-type="firstName">firstName</td>
+    tr.innerHTML =
+        `<td data-type="firstName">firstName</td>
     <td data-type="lastName">lastName</td>
     <td data-type="capsule">capsule</td>
     <td data-type="gender">gender</td>
@@ -79,19 +79,19 @@ function displayingData(){
 
     studentsContainer.appendChild(tr);
 
-    for (let student in studentsData){
+    for (let student in studentsData) {
         let tr = document.createElement('tr');
 
-        tr.innerHTML = 
-        `<td data-row="${student}" data-type="firstName">${studentsData[student].firstName}</td>
+        tr.innerHTML =
+            `<td data-row="${student}" data-type="firstName">${studentsData[student].firstName}</td>
         <td data-row="${student}" data-type="lastName">${studentsData[student].lastName}</td>
         <td data-row="${student}" data-type="capsule">${studentsData[student].capsule}</td>
         <td data-row="${student}" data-type="gender">${studentsData[student].gender}</td>
         <td data-row="${student}" data-type="age">${studentsData[student].age}</td>
         <td data-row="${student}" data-type="city">${studentsData[student].city}</td>
         <td data-row="${student}" data-type="hobby">${studentsData[student].hobby}</td>
-        <button data-row="${student}" class='edit-button'>Edit</button>
-        <button data-row="${student}" class='delete-button'>Delete</button>
+        <button data-row="${student}" class='edit-button'>${studentsData[student].editButton}</button>
+        <button data-row="${student}" class='delete-button'>${studentsData[student].deleteButton}</button>
         `
 
         studentsContainer.appendChild(tr);
@@ -99,46 +99,70 @@ function displayingData(){
 }
 
 
-function deleteRow(rowNum){
+function deleteRow(rowNum) {
     delete studentsData[rowNum];
     displayingData();
 }
 
 // on event click on edit button
-function editRow(rowNum){
-    debugger;
-    
+function editRow(rowNum) {
     let studentData = studentsData[rowNum];
-    
-    studentsData[rowNum] = {id: studentData.id , firstName: `<input type="text" value="${studentData.firstName}">`,
-    lastName: `<input type="text" value="${studentData.lastName}">`, capsule: `<input type="text" value="${studentData.capsule}">`,
-    age: `<input type="text" value="${studentData.age}">`, city: `<input type="text" value="${studentData.city}">`,
-    gender: `<input type="text" value="${studentData.gender}">`, hobby: `<input type="text" value="${studentData.hobby}">`}
-    
-    let buttons = document.querySelectorAll('button')
 
-    
-    
-    
-    let editButtonOrDelCount = 0;
-    for (let btn of buttons){ // pulling the right buttons to change.
-        if (btn.getAttribute('data-row') == rowNum){
-            switch (editButtonOrDelCount){
-                case 0:
-                    btn.innerText = 'Cancel';
-                    btn.style.backgroundColor = 'yellow';
-                    editButtonOrDelCount++;
-                    break;
-                case 1:
-                    btn.innerText = 'Confirm';
-                    btn.style.backgroundColor = 'red';
-                    break;
-            }
-        }
+    studentsData[rowNum] = {
+        id: studentData.id,
+        firstName: `<input type="text" data-type="firstName" value="${studentData.firstName}">`,
+        lastName: `<input type="text" data-type="lastName" value="${studentData.lastName}">`,
+        capsule: `<input type="text" data-type="capsule" value="${studentData.capsule}">`,
+        age: `<input type="text" data-type="age" value="${studentData.age}">`,
+        city: `<input type="text" data-type="city" value="${studentData.city}">`,
+        gender: `<input type="text" data-type="gender" value="${studentData.gender}">`,
+        hobby: `<input type="text" data-type="hobby" value="${studentData.hobby}">`,
+        editButton: 'Confirm',
+        deleteButton: 'Cancel'
     }
 
-displayingData(); /// updating
-    
+    displayingData(); /// updating
+    return studentData;
+}
+
+function applyEditOnRow(rowNum, studentData) {
+    console.log(studentData)
+    let inputs = document.querySelectorAll('input')
+    let firstOne = true;
+    for (let input of inputs) {
+        if (!firstOne) {
+            if (input.value != input.getAttribute('value')) {
+                let type = input.getAttribute('data-type');
+                studentData[type] = input.value;
+            }
+        } else firstOne = false; // ignores search input
+    }
+
+    studentsData[rowNum] = studentData;
+    displayingData();
 }
 
 
+studentsContainer.addEventListener('click', (e) => {
+    let row = e.target.getAttribute('data-row');
+    if (e.target.innerText == 'Confirm') {
+        console.log('confirm');
+        applyEditOnRow(row, editedStudent);
+    }
+    if (e.target.innerText == 'Edit') {
+        console.log('edit');
+
+        editedStudent = editRow(row);
+    }
+    if (e.target.innerText == 'Delete') {
+        console.log('del');
+
+        deleteRow(row);
+    }
+    if (e.target.innerText == 'Cancel') {
+        console.log('cancel');
+
+        displayingData();
+    }
+
+})
